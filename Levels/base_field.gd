99f89b2a6361a696a0495bcacd071bf326e49330
Nathan <nathan.casimyr@gmail.com>
2026-09-23 16:24:41 -0400

    remove FieldTemplate.tscn

--- Levels/base_field.gd
+++ Levels/base_field.gd
@@ -3,5 +3,7 @@ extends Node2D
 @export var spawn_positon: Vector2 = Vector2.ZERO
 @export var bonus_points: int = 40
 
+func _ready():
+	$BadTimer.start()
 func _process(delta: float) -> void:
 	Globals.field_time_left = $BadTimer.time_left
