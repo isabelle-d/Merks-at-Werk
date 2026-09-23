d1b2c64e098420a8ff60cc4af83897d75f8e9968
Nathan <nathan.casimyr@gmail.com>
2026-09-23 17:35:13 -0400

    Added a number of fields to BaseField. timer_enabled: determines whether field_timer is enabled or not. spawn_timer: timer for missile spawning. missile_scene: scene that contains missiles to spawn in. Also added basic missile spawner for BaseField. Works by picking a corner for the missile and spawning from there.

--- Levels/base_field.gd
+++ Levels/base_field.gd
@@ -1,9 +1,35 @@
+class_name BaseField
 extends Node2D
 
-@export var spawn_positon: Vector2 = Vector2.ZERO
-@export var bonus_points: int = 40
+@export var spawn_positon : Vector2 = Vector2.ZERO
+@export var bonus_points  : int = 40
+@export var timer_enabled : bool = false
+@export var field_timer   : Node
+@export var spawn_timer   : Node
+@export var missile_scene : PackedScene
 
 func _ready():
-	$BadTimer.start()
+	if timer_enabled:
+		print("start timer")
+		field_timer.start()
+		print("start timer")
+	spawn_timer.start()
+
 func _process(delta: float) -> void:
-	Globals.field_time_left = $BadTimer.time_left
+	if timer_enabled:
+		Globals.field_time_left = field_timer.time_left
+	
+
+
+func _on_SpawnTimer_timeout() -> void:
+	var missile = missile_scene.instantiate()
+	var x_edge = 1 if randi() % 2 else -1
+	var y_edge = -1 if randi() % 2 else 1
+	var edge_pos : Vector2 = DisplayServer.screen_get_size()
+	edge_pos.x *= x_edge
+	edge_pos.y *= y_edge
+	missile.global_position = get_viewport().get_camera_2d().global_position + edge_pos
+	missile.axis = (Globals.player_position - missile.global_position).normalized()
+	add_child(missile)
+	print("Booyakasha! gp = ", missile.global_position, ", edge = ", edge_pos)
+	spawn_timer.start()
--- Main/main.gd
+++ Main/main.gd
@@ -2,13 +2,13 @@ extends Node2D
 
 @onready var level_container = $LevelContainer
 var current_level
+var stations_cleared = false
 
-signal level_clear
+
 
 func _ready() -> void:
 	pass
 
-var stations_cleared = false
 func _process(delta: float) -> void:
 	if !stations_cleared and len(get_tree().get_nodes_in_group("station")) == 0:
 		stations_cleared = true
